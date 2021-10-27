a970fe10803873affcf9dad1dc04887acae4d9f8
Person1782 <84298337+Person1782@users.noreply.github.com>
2021-10-27 16:01:04 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,47 +1,78 @@
-var names_of_people = [];
-    
-function submit()
-{
-    var GuestName = document.getElementById("name1").value;
-	names_of_people.push(GuestName);
-	console.log(GuestName);    
-    console.log(names_of_people);
-    var lenght_of_name = names_of_people.length;
-    console.log(lenght_of_name);
-	document.getElementById("display_name").innerHTML=names_of_people.toString();
-   }
+// Create a reference for the canvas
+var canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
 
+img_width = 300;
+img_height = 100;
 
+var img_image;
 
-function show()
-{
-	var i= names_of_people.join("<br>");
-	console.log(names_of_people);
-	document.getElementById("p1").innerHTML=i.toString();
-	document.getElementById("sort_button").style.display="block";
-	}
+img_x = 100;
+img_y = 100;
 
-
-function sorting()
-	{
-		names_of_people.sort();
-		var i= names_of_people.join("<br>");
-		console.log(names_of_people);		
-		document.getElementById("sorted").innerHTML=i.toString();
-		}
-
-
-function searching()
-{
-	var s= document.getElementById("s1").value;
-	var found=0;
-	var j;
-	for(j=0; j<names_of_people.length; j++)
-		{
-			if(s==names_of_people[j]){
-				found=found+1;
-			}	
-		}
-	document.getElementById("p2").innerHTML="name found "+found+" time/s";
-	console.log("found name "+found+" time/s");
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
 }
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+//Write a code to grab the key-pressed event
+	window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		//write a code to check the type of key pressed
+		{
+		aplhabetkey();
+		document.getElementById("d1").innerHTML="You pressed an alphabet key."
+		console.log("alphabet");
+		}
+	else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+		spacialkey();
+		document.getElementById("d1").innerHTML="You pressed the space key";
+		numberkey();
+		document.getElementById("d1").innerHTML="You presseda number key";
+		arrowkey();
+		document.getElementById("d1").innerHTML="You pressed an arrow key";
+	}
+}
+
+function aplhabetkey()
+{
+	//upload respective image with the message.
+	img_image = "Alpkey.png" 
+	add();
+
+}
+function numberkey()
+{
+	img_image = "numkey.png" 
+	add();
+}
+function arrowkey()
+{
+	img_image = "Arr.png" 
+	add();
+}
+function spacialkey()
+{
+	img_image = "spkey.png" 
+	add();
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
